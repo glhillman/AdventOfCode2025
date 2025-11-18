b642c4669b0c56d22e1fd4629adaa543586706cb
glhillman <glhillman@gmail.com>
2025-11-18 09:51:00 -0700

    Initial - no custom code (git test)

---
+++ Day01/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+
+    public DayClass()
+    {
+        LoadData();
+    }
+
+    public void Part1()
+    {
+
+        long rslt = 0;
+
+        Console.WriteLine("Part1: {0}", rslt);
+    }
+
+    public void Part2()
+    {
+
+        long rslt = 0;
+
+        Console.WriteLine("Part2: {0}", rslt);
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\test.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                // Process the string
+            }
+
+            file.Close();
+        }
+    }
+
+}

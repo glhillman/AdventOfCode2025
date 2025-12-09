dc4c233ccaf4546949c9681af95b25903f446d65
glhillman <glhillman@gmail.com>
2025-12-09 05:46:26 -0700

    Day08 Complete

---
+++ Day08/Junction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day08
+{
+    public class Junction
+    {
+        public Junction(int iD, long x, long y, long z)
+        {
+            ID = iD;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int ID { get; private set; }
+        public long X { get; private set; }
+        public long Y { get; private set; }
+        public long Z { get; private set; }
+        public long DistanceTo(Junction other)
+        {
+            // true distance is the square root of this full expression, but is unnecessary for our purposes. Nice to stay with longs & not doubles
+            return ((X - other.X) * (X - other.X)) + ((Y - other.Y) * (Y - other.Y)) + ((Z - other.Z) * (Z - other.Z));
+        }
+        public override string ToString()
+        {
+            return string.Format("ID: {0}, X: {1}, Y: {2}, Z: {3}", ID, X, Y, Z);
+        }
+    }
+
+    public class DistRec
+    {
+        public DistRec(int id1, int id2, long dist)
+        {
+            ID1 = id1;
+            ID2 = id2;
+            Dist = dist;
+        }
+        public int ID1 { get; private set; }
+        public int ID2 { get; private set; }
+        public long Dist { get; private set; }
+        public override string ToString()
+        {
+            return string.Format("ID1: {0}, ID2: {1}, Dist: {2}", ID1, ID2, Dist);
+        }
+    }
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,166 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day08;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+internal class DayClass
+{
+    List<Junction> _junctions = new();
+    List<DistRec> _distances = new();
+    List<HashSet<int>> _circuits = new();
+    public DayClass()
+    {
+        LoadData();
+        CalculateDistances();
+        _distances.Sort((a, b) => { long diff = a.Dist - b.Dist; return diff < 0 ? -1 : diff > 0 ? 1 : 0; });
+    }
+
+    public void Part1()
+    {
+         ProcessRecords(0, 999, false);
+
+        long rslt = _circuits[0].Count * _circuits[1].Count * _circuits[2].Count;
+
+        Console.WriteLine("Part1: {0}", rslt);
+    }
+
+    public void Part2()
+    {
+        long rslt = ProcessRecords(1000, 5000, true);// solution will be found before this many loops
+
+        Console.WriteLine("Part2: {0}", rslt);
+    }
+
+    private long ProcessRecords(int start, int end, bool runningMerge)
+    {
+        long result = 0;
+        long holdID1 = _distances[0].ID1;
+        long holdID2 = _distances[0].ID2;
+
+        for (int i = start; i <= end; i++)
+        {
+            bool recUsed = false;
+            
+            DistRec rec = _distances[i];
+            if (_circuits.Count > 0 && _circuits[0].Count == 1000) // 1000 indicates all connections made
+            {
+                Junction j1 = _junctions.First(r => r.ID == holdID1);
+                Junction j2 = _junctions.First(r => r.ID == holdID2);
+                result = j1.X * j2.X;
+                break;
+            }
+            else
+            {
+                holdID1 = rec.ID1;
+                holdID2 = rec.ID2;
+            }
+                // check all the circuit lists for either/both ids
+                foreach (HashSet<int> circuit in _circuits)
+                {
+                    bool contains1 = circuit.Contains(rec.ID1);
+                    bool contains2 = circuit.Contains(rec.ID2);
+
+                    if (contains1 && contains2)
+                    {
+                        recUsed = true; // do nothing with it - already used
+                    }
+                    else if (contains1)
+                    {
+                        circuit.Add(rec.ID2);
+                        recUsed = true;
+                    }
+                    else if (contains2)
+                    {
+                        circuit.Add(rec.ID1);
+                        recUsed = true;
+                    }
+                }
+            if (!recUsed)
+            {
+                HashSet<int> newCircuit = new();
+                newCircuit.Add(rec.ID1);
+                newCircuit.Add(rec.ID2);
+                _circuits.Add(newCircuit);
+            }
+
+            if (runningMerge || i == end)
+            {
+                // merge the circuits for overlapping entries
+                for (int ii = 0; ii < _circuits.Count; ii++)
+                {
+                    for (int j = 0; j < _circuits.Count; j++)
+                    {
+                        if (ii != j)
+                        {
+                            foreach (int box in _circuits[ii])
+                            {
+                                if (_circuits[j].Contains(box))
+                                {
+                                    foreach (int box2 in _circuits[ii])
+                                    {
+                                        _circuits[j].Add(box2);
+                                    }
+                                    _circuits[ii].Clear();
+                                }
+                            }
+                        }
+                    }
+                }
+
+                _circuits.Sort((a, b) => b.Count - a.Count);
+                int firstEmpty = _circuits.FindIndex(r => r.Count == 0);
+                if (firstEmpty > 0)
+                {
+                    _circuits.RemoveRange(firstEmpty, _circuits.Count - firstEmpty);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    public void CalculateDistances()
+    {
+        for (int i = 0; i < _junctions.Count-1; i++)
+        {
+            for (int j = i + 1; j < _junctions.Count; j++)
+            {
+                long dist = _junctions[i].DistanceTo(_junctions[j]);
+                _distances.Add(new DistRec(_junctions[i].ID, _junctions[j].ID, dist));
+            }
+        }
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            int id = 1;
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                _junctions.Add(new Junction(id++, long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2])));
+            }
+
+            file.Close();
+        }
+    }
+
+}

a27d9cc5d8fb9d0e098a419383b5220120071f0a
glhillman <glhillman@gmail.com>
2025-12-17 08:00:44 -0700

    Day10.1 Done. 10.2 ? Maybe someday

---
+++ Day10/Machine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day10
+{
+    public class Machine
+    {
+        public Machine((int target, int targetMax) targetInfo, List<int> buttons)
+        {
+            Target = targetInfo.target;
+            TargetMax = targetInfo.targetMax;
+            Buttons = buttons;
+
+            if (_nBits.Count == 0)
+            {
+                for (int i = 0; i <= 8192; i++)
+                {
+                    int nBits = 0;
+                    int num = i;
+                    while (num > 0)
+                    {
+                        nBits += num & 1;
+                        num >>= 1;
+                    }
+                    _nBits.Add((i, nBits));
+                }
+            }
+
+            // sort by ascending nBits, to try fewest button combinations first
+            _nBits.Sort((a, b) => a.nBits < b.nBits ? -1 : a.nBits > b.nBits ? 1 : a.num - b.num);
+
+        }
+        public static List<(int num, int nBits)> _nBits = new();
+        public int Target { get; private set; }
+        public int TargetMax { get; private set; }
+        public List<int> Buttons { get; set; }
+        public int LowestPresses()
+        {
+            int testTarget = 0;
+            while (testTarget != Target)
+            {
+                foreach ((int num, int nBits) testNum in _nBits)
+                {
+                    if (testNum.num <= TargetMax)
+                    {
+                        testTarget = 0;
+                        int num = testNum.num;
+                        // bit positions in num are used as index into button array
+                        int offset = 0;
+                        while (num > 0)
+                        {
+                            if ((num & 1) == 1)
+                            {
+                                testTarget ^= Buttons[offset];
+                            }
+                            num >>= 1;
+                            offset++;
+                        }
+                        if (testTarget == Target)
+                        {
+                            return testNum.nBits;
+                        }
+                    }
+                }
+            }
+
+            return -1; // Placeholder return value
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Target: {0}/{1}, Buttons: ", Target, TargetMax);
+            for (int i = 0; i < Buttons.Count; i++)
+            {
+                sb.Append(Buttons[i]);
+                if (i < Buttons.Count - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day10/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.PortableExecutable;
+using Day10;
+
+DayClass day = new DayClass();
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+day.Part1();
+day.Part2();
+
+watch.Stop();
+Console.WriteLine("Execution Time: {0} ms", watch.ElapsedMilliseconds);
+
+Console.Write("Press Enter to continue...");
+Console.ReadLine();
+
+public class DayClass
+{
+    public List<Day10.Machine> _machines = new();
+    public DayClass()
+    {
+        LoadData();
+    }
+
+    public void Part1()
+    {
+        int sum = 0;
+
+        foreach (Day10.Machine machine in _machines)
+        {
+            sum += machine.LowestPresses();
+        }
+
+        Console.WriteLine("Part1: {0}", sum);
+    }
+
+    public void Part2()
+    {
+
+        long rslt = 0;
+
+        Console.WriteLine("Part2: {0}", rslt);
+    }
+
+    private void LoadData()
+    {
+        string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\input.txt";
+
+        if (File.Exists(inputFile))
+        {
+            string? line;
+            StreamReader file = new StreamReader(inputFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] parts = line.Split(' ');
+                int target = ParseTarget(parts[0]);
+                List<int> buttons = ParseButtons(parts);
+                _machines.Add(new Day10.Machine((target, (1 << buttons.Count) - 1), buttons));
+            }
+
+            file.Close();
+        }
+    }
+    public int ParseTarget(string s)
+    {
+        // comes in as [###..#.#..], need to convert to bitmask
+        s = s.Trim('[', ']');
+        int mask = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '#')
+            {
+                mask |= 1 << i;
+            }
+        }
+
+        return mask;
+    }
+
+    public List<int> ParseButtons(string[] parts)
+    {
+        List<int> buttons = new();
+        for (int i = 1; i < parts.Length - 1; i++)
+        {
+            string s = parts[i].Trim('(', ')');
+            string[] offsets = s.Split(',');
+            int button = 0;
+            foreach (string offset in offsets)
+            {
+                int iOffset = int.Parse(offset);
+                button |= (1 << iOffset);
+            }
+            buttons.Add(button);
+        }
+
+        return buttons;
+    }
+}
